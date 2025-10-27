ecdbacac4eba940c9ec9ef1d7c595244fca1ee32
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-27 14:02:10 +0100

    format document

--- assets/main.js
+++ assets/main.js
@@ -13,13 +13,30 @@ Tools:
 /* Funzioni */
 
 // Funzione markup
+function getMarkup(image, date, title) {
+  return   `<div class="col">
+                <div class="card p-3 h-100 position-relative" style="width: 18rem;">
+                    <div class="position-absolute top-0 start-50 translate-middle"><img src="./assets/img/pin.svg" alt=""></div>
+                    <img src="${image}" alt="">
+                    <div class="pt-3">
+                        <span class="text-secondary">${date}</span>
+                        <h2 class="text-uppercase fw-bold">${title}</h2>
+                    </div>
+                </div>
+            </div>`;
+}
 
 // ======================================================================
 // ======================================================================
 
 /* Variabili */
 
+// Contenitore delle card
+let cards = [];
+console.log(cards);
+
 // Spazio div dell'HTML
+const cardsField = document.getElementById("cards");
 
 /* Collegamento API */
 
@@ -27,30 +44,17 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
   .then((response) => response.json())
   .then((data) => {
 
-    function getMarkup(image, date, title) {
-      return `<div class="col">
-        <div class="card p-3 h-100 position-relative" style="width: 18rem;">
-            <div class="position-absolute top-0 start-50 translate-middle"><img src="./assets/img/pin.svg" alt=""></div>
-                <img src="${image}" alt="">
-                <div class="pt-3">
-                    <span class="text-secondary">${date}</span>
-                    <h2 class="text-uppercase fw-bold">${title}</h2>
-                </div>
-        </div>
-    </div>`;
-    }
-    let cards = [];
-    console.log(cards);
-    const cardsField = document.getElementById("cards");
-
+    // Inserimento elementi data nell'array
     data.forEach((thisEl) => {
       cards.push(thisEl);
     });
 
+    /* Ciclo inserimeto dati in pagina */
+
     if (cards.length !== 0) {
       for (let i = 0; i < cards.length; i++) {
         const thisCard = cards[i];
-        
+
         cardsField.innerHTML += getMarkup(
           thisCard.url,
           thisCard.date,
@@ -63,7 +67,4 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
     console.error(error);
   });
 
-// ======================================================================
-// ======================================================================
 
-/* Ciclo inserimeto dati in pagina */
