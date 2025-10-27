28dd702675d92f45457e81dff91804c661327e4f
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-27 12:41:22 +0100

    add tasks

--- assets/main.js
+++ assets/main.js
@@ -4,7 +4,16 @@ Tools:
 - const / let
 - for
 - fetch API
+- functions
 */
 
 // ====================================================================
 // ====================================================================
+
+/* Funzioni */
+
+/* Variabili */
+
+/* Collegamento API */
+
+/* Ciclo inserimeto dati in pagina */
